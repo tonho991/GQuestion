00a43706a7dcd09919f4b897ed9bedd7670aeaea
tonho991 <tonhoteca@gmail.com>
2023-03-26 15:46:41 -0400

    GQ

--- routes/generate.js
+++ routes/generate.js
@@ -8,7 +8,9 @@ const crypto = require('crypto');
 const {
   checkRequests
 } = require('../util/Firebase');
-const { openAI } = require('../constants/api');
+const {
+  openAI
+} = require('../constants/api');
 
 const configuration = new Configuration({
   apiKey: openAI.apiKey,
@@ -28,8 +30,9 @@ app.post('/', async (req, res) => {
     response
   } = decodedData;
 
-  if(!text || !questions || !response){
+  if (!text || !questions || !response) {
     res.status(401).json({
+      status: "error",
       message: "Parâmetros inválidos."
     });
   }
@@ -37,18 +40,28 @@ app.post('/', async (req, res) => {
   const checkData = await checkRequests(userId);
   if (!checkData.success) {
     res.status(401).json({
+      status: "error",
       message: checkData.msg
     });
     return;
   }
-  
-  
-    const genText = await generateText(text, questions, response);
 
+
+  generateText(text, questions, response)
+  .then((result) => {
     res.status(200).json({
-      message: "OK",
-      text: generateText
+      status: "OK",
+      message: "Perguntas geradas com sucesso",
+      text: result
     });
+  }).catch((err) => {
+    res.status(200).json({
+      status: "error",
+      message: "Erro ao gerar sua pergunta.",
+    });
+  });
+
+
 
 
 });
@@ -60,28 +73,35 @@ function md5(str) {
 }
 
 
-async function generateText(Text, Quest, Resp) {
-  return new Promise(async (res, rej) => {
+async function generateText(text, question, answer) {
+  try {
     const openai = new OpenAIApi(configuration);
-    var cmd = `Gere ${Quest} sobre o seguinte texto, adicione a tag <strong> na pergunta:: ${Text}`;
-    if (Resp) {
-      cmd = `Gere ${Quest} sobre o seguinte texto, e adicione a resposta em baixo, adicione também a tag <strong> na pergunta: ${Text}`;
+
+    var prompt = `Gere ${question} sobre o seguinte texto, adicione a tag <strong> na pergunta: ${text}`;
+    if (answer) {
+      prompt = `Gere ${question} sobre o seguinte texto, adicione a resposta em baixo, adicione também a tag <strong> na pergunta: ${text}`;
     }
+
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: cmd,
+      prompt: prompt,
       temperature: 0,
-      max_tokens: 100,
+      max_tokens: 2048,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
       stop: ["input:"]
     });
-    var result = completion.data.choices[0].text;
-    res(result);
-  });
+    console.log(completion.data)
+    const result = completion.data.choices[0].text;
 
+    return result;
+  } catch (error) {
+    console.error("Error while generating text:", error);
+    throw error;
+  }
 }
+
 
 
 module.exports = {
